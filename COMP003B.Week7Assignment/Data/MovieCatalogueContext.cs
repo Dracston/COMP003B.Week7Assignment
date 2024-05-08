cb814963341f41635dd61af45f0d78d4bd8c34dc
Logan Jones <lpj11101@gmail.com>
2024-05-07 19:54:38 -0700

    Added MovieCatalogueContext and  updated Json

---
+++ COMP003B.Week7Assignment/Data/MovieCatalogueContext.cs
@@ -0,0 +1,19 @@
+﻿using COMP003B.Week7Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Week7Assignment.Data
+{
+    public class MovieCatalogueContext :DbContext
+    {
+        public MovieCatalogueContext(DbContextOptions<MovieCatalogueContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<IMDB> IMDBs { get; set; }
+
+
+    }
+}

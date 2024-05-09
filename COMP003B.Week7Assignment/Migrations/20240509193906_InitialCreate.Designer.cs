3e08599e289719cf65ee9694e21249eaa070f0d0
Logan Jones <lpj11101@gmail.com>
2024-05-09 12:44:42 -0700

    Updated the Database Correctly. Finished project

--- COMP003B.Week7Assignment/Migrations/20240509193906_InitialCreate.Designer.cs
+++ COMP003B.Week7Assignment/Migrations/20240509193906_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using COMP003B.Week7Assignment.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace COMP003B.Week7Assignment.Migrations
 {
     [DbContext(typeof(MovieCatalogueContext))]
-    partial class MovieCatalogueContextModelSnapshot : ModelSnapshot
+    [Migration("20240509193906_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
